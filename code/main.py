593ad1d549e04528689bd079f6ae8eae733f530f
reacher-l <45810596+reacher-l@users.noreply.github.com>
2021-04-08 23:50:43 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,28 +2,30 @@ from ai_hub import inferServer
 import json
 import base64
 import cv2
+from PIL import Image
+from io import BytesIO
 import numpy as np
 import torch
 from torch.autograd import Variable as V
 import base64
 from model import UnetIBN
 import torch.nn.functional as F
-import tifffile
-from io import BytesIO
-
 
 
 class myInfer(inferServer):
     def __init__(self, model):
         super().__init__(model)
+
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.device = device
         self.mean = torch.tensor([0.19014559, 0.23549616, 0.22207538, 0.52786024]).unsqueeze(0).unsqueeze(2).unsqueeze(3).to(device)
         self.std = torch.tensor([0.12677416, 0.12194977, 0.1188112, 0.19847064]).unsqueeze(0).unsqueeze(2).unsqueeze(3).to(device)
+        data = torch.ones((1, 4, 256, 256)).to(device)
         self.model = model.to(device)
-        checkpoint = torch.load('best.pth',map_location=device)
+        checkpoint = torch.load('1.pth',map_location=device)
         self.model.load_state_dict(checkpoint['state_dict'])
         self.model.eval()
+
 
     # 数据前处理
     def pre_process(self, data):
@@ -32,7 +34,7 @@ class myInfer(inferServer):
         bast64_data = img.encode(encoding='utf-8')
         img = base64.b64decode(bast64_data)
         bytesIO = BytesIO()
-        img =  tifffile.imread(BytesIO(bytearray(img)))
+        img = np.asarray(Image.open(BytesIO(bytearray(img))),dtype=np.float32)
         return img
 
 
@@ -47,11 +49,9 @@ class myInfer(inferServer):
     # 如需自定义，可覆盖重写
     def predict(self, data):
         with torch.no_grad():
-            img = torch.from_numpy(data).unsqueeze(0).cuda()
+            img = torch.from_numpy(data.transpose(2, 0, 1)).unsqueeze(0).cuda()
             img = (img / 255. - self.mean) / self.std
-            # output2 = self.model(img)
-            output3 = self.model(img)
-            output = self.model(img)
+            output = self.model (img)
             output = torch.argmax(output,dim=1)
             output = output+1
         pred = output.squeeze().cpu().data.numpy()
@@ -60,6 +60,6 @@ class myInfer(inferServer):
 
 
 if __name__ == "__main__":
-    model = UnetIBN(encoder_pretrained=False)
-    my_infer = myInfer(model)
+    mymodel = UnetIBN(encoder_pretrained=False,classes=10)
+    my_infer = myInfer(mymodel)
     my_infer.run(debuge=True)  # 默认为("127.0.0.1", 80)，可自定义端口，如用于天池大赛请默认即可，指定debuge=True可获得更多报错信息
